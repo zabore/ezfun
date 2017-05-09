fadf5b7dc1f376d057203004e7f54129a645f6eb
Emily C. Zabor <emily.zabor@gmail.com>
2017-05-09 11:50:11 -0400

    Add files via upload

--- R/uvsurv.R
+++ R/uvsurv.R
@@ -29,50 +29,58 @@ uvsurv <- function(contvars, catvars, event, time, test, dat) {
 
   mats <- vector('list', length(catvars) + length(contvars))
   nc <- length(contvars)
-
-  for(k in 1:nc) {
-
-    mats[[k]] <- matrix(NA, nrow = 1, ncol = 5)
-    c1 <- coxph(Surv(dat[, time], dat[, event]) ~ dat[, contvars[[k]]])
-    mats[[k]][1, 2] <- 'NA'
-    mats[[k]][1, 3] <- 'NA'
-    mats[[k]][1, 4] <- paste0(round(summary(c1)$conf.int[1, "exp(coef)"], 2), " (",
-                          round(summary(c1)$conf.int[1, "lower .95"], 2), "-",
-                          round(summary(c1)$conf.int[1, "upper .95"], 2), ")")
-    mats[[k]][1, 5] <- round(summary(c1)$sctest["pvalue"], 3)
-    mats[[k]][1, 5][mats[[k]][1, 5] == '0'] <- "<.001"
-    mats[[k]] <- as.data.frame(mats[[k]], stringsAsFactors = F)
-    mats[[k]][, 1] <- as.character(mats[[k]][, 1])
-    mats[[k]][, 1] <- paste(contvars[k])
+  
+  if(!is.null(contvars)) {
+    
+    for(k in 1:nc) {
+      
+      mats[[k]] <- matrix(NA, nrow = 1, ncol = 5)
+      c1 <- coxph(Surv(dat[, time], dat[, event]) ~ dat[, contvars[[k]]])
+      mats[[k]][1, 2] <- 'NA'
+      mats[[k]][1, 3] <- 'NA'
+      mats[[k]][1, 4] <- paste0(round(summary(c1)$conf.int[1, "exp(coef)"], 2), " (",
+                                round(summary(c1)$conf.int[1, "lower .95"], 2), "-",
+                                round(summary(c1)$conf.int[1, "upper .95"], 2), ")")
+      mats[[k]][1, 5] <- round(summary(c1)$sctest["pvalue"], 3)
+      mats[[k]][1, 5][mats[[k]][1, 5] == '0'] <- "<.001"
+      mats[[k]] <- as.data.frame(mats[[k]], stringsAsFactors = F)
+      mats[[k]][, 1] <- as.character(mats[[k]][, 1])
+      mats[[k]][, 1] <- paste(contvars[k])
+    }
+    
   }
 
-  for(k in 1:length(catvars)) {
-    mats[[k + nc]] <- matrix('', nrow = length(levels(factor(dat[, catvars[[k]]]))) + 1, ncol = 5)
-    s1 <- Surv(dat[, time], dat[, event]) ~ factor(dat[, catvars[[k]]])
-    f1 <- survfit(s1)
-    c1 <- coxph(s1)
-
-    for(i in 1:length(levels(factor(dat[, catvars[[k]]])))) {
-      mats[[k + nc]][i + 1, 1] <- paste(levels(as.factor(dat[, catvars[[k]]]))[i])
-      mats[[k + nc]][i + 1, 2] <- paste0(round(summary(f1)$table[i, 'median'], 2), " (",
-                                         round(summary(f1)$table[i, "0.95LCL"], 2), "-",
-                                         round(summary(f1)$table[i, "0.95UCL"], 2), ")")
-      mats[[k + nc]][i + 1,3] <- paste0(round(summary(f1, times = test)$surv, 2), " (",
-                                        round(summary(f1, times = test)$lower, 2), "-",
-                                        round(summary(f1,times=test)$upper,2), ")")[i]
-      if(i == 1) {
-        mats[[k + nc]][i + 1, 4] <- '1.00'
-      } else if(i > 1) {
-        mats[[k + nc]][i + 1, 4] <- paste0(round(summary(c1)$conf.int[i - 1, "exp(coef)"], 2), " (",
-                                           round(summary(c1)$conf.int[i - 1, "lower .95"], 2), "-",
-                                           round(summary(c1)$conf.int[i - 1, "upper .95"],2), ")")
+  if(!is.null(catvars)) {
+    
+    for(k in 1:length(catvars)) {
+      mats[[k + nc]] <- matrix('', nrow = length(levels(factor(dat[, catvars[[k]]]))) + 1, ncol = 5)
+      s1 <- Surv(dat[, time], dat[, event]) ~ factor(dat[, catvars[[k]]])
+      f1 <- survfit(s1)
+      c1 <- coxph(s1)
+      
+      for(i in 1:length(levels(factor(dat[, catvars[[k]]])))) {
+        mats[[k + nc]][i + 1, 1] <- paste(levels(as.factor(dat[, catvars[[k]]]))[i])
+        mats[[k + nc]][i + 1, 2] <- paste0(round(summary(f1)$table[i, 'median'], 2), " (",
+                                           round(summary(f1)$table[i, "0.95LCL"], 2), "-",
+                                           round(summary(f1)$table[i, "0.95UCL"], 2), ")")
+        mats[[k + nc]][i + 1,3] <- paste0(round(summary(f1, times = test)$surv, 2), " (",
+                                          round(summary(f1, times = test)$lower, 2), "-",
+                                          round(summary(f1,times=test)$upper,2), ")")[i]
+        if(i == 1) {
+          mats[[k + nc]][i + 1, 4] <- '1.00'
+        } else if(i > 1) {
+          mats[[k + nc]][i + 1, 4] <- paste0(round(summary(c1)$conf.int[i - 1, "exp(coef)"], 2), " (",
+                                             round(summary(c1)$conf.int[i - 1, "lower .95"], 2), "-",
+                                             round(summary(c1)$conf.int[i - 1, "upper .95"],2), ")")
+        }
       }
+      mats[[k + nc]][1, 5] <- round(summary(c1)$sctest["pvalue"], 3)
+      mats[[k + nc]][1, 5][mats[[k + nc]][1, 5] == '0'] <- "<.001"
+      mats[[k + nc]] <- as.data.frame(mats[[k + nc]], stringsAsFactors = F)
+      mats[[k + nc]][, 1] <- as.character(mats[[k + nc]][, 1])
+      mats[[k + nc]][1, 1] <- paste(catvars[k])
     }
-    mats[[k + nc]][1, 5] <- round(summary(c1)$sctest["pvalue"], 3)
-    mats[[k + nc]][1, 5][mats[[k + nc]][1, 5] == '0'] <- "<.001"
-    mats[[k + nc]] <- as.data.frame(mats[[k + nc]], stringsAsFactors = F)
-    mats[[k + nc]][, 1] <- as.character(mats[[k + nc]][, 1])
-    mats[[k + nc]][1, 1] <- paste(catvars[k])
+    
   }
 
   mats <- do.call(rbind,mats)

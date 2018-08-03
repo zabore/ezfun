210b738a465c995d9857ab2de04aa9dd6bbca0af
Emily Zabor <emily.zabor@gmail.com>
2018-08-03 17:06:01 -0400

    getting MSK color palette to work in right order

--- R/msk_color_palette.R
+++ R/msk_color_palette.R
@@ -1,128 +1,90 @@
-#' List of branded MSK colors
-msk_colors <- c(
-  `MSK blue` = "#007CBA",
-  `MSK orange` = "#DF4602",
-  `Dark blue` = "#006098",
-  `Medium blue` = "#40B4E5",
-  `Light blue` = "#8FC7E8",
-  `Dark orange` = "#C24D00",
-  `Medium orange` = "#F29934",
-  `Light orange` = "#F6C65B",
-  `Dark violet` = "#83276B",
-  `Light violet` = "#B687B8",
-  `Dark magenta` = "#E92076",
-  `Light magenta` = "#EFB3CB",
-  `Dark green` = "#4C8B2B",
-  `Light green` = "#A6CE39",
-  `Dark turquoise` = "#006C68",
-  `Light turquoise` = "#009490",
-  `Dark yellow` = "#FFC20E",
-  `Light yellow` = "#FFE100",
-  `Dark gray` = "#9E9E98",
-  `Light gray` = "#D5D4C7")
-
-
-#' Function to extract MSK colors as hex codes
-#'
-#' @param ... Character names of msk_colors
-#'
-#' @return Returns the hexadecimal code associated with the named color
-#'
-#' @example
-#'
-#' msk_cols("Dark blue")
+#' Complete list of MSK branded color palettes
 #'
 #' @export
-#'
-msk_cols <- function(...) {
-  cols <- c(...)
 
-  if (is.null(cols))
-    return (msk_colors)
-
-  msk_colors[cols]
-}
-
-
-#' List of color palettes I think work okay for visualizations based on these
 msk_palettes <- list(
-  `main`  = msk_cols("MSK blue", "MSK orange", "Medium blue",
-                     "Medium orange", "Light blue", "Light orange",
-                     "Dark blue", "Dark orange"),
-  `blues`  = msk_cols("MSK blue", "Medium blue", "Light blue", "Dark blue"),
-  `oranges`   = msk_cols("MSK orange", "Medium orange", "Light orange",
-                         "Dark orange"),
-  `contrast` = msk_cols("MSK blue", "MSK orange", "Dark violet", "Dark green",
-                        "Dark magenta", "Dark turquoise", "Dark yellow",
-                        "Light violet", "Light green", "Light magenta",
-                        "Light turquoise", "Light yellow")
+  all = c("#007CBA", "#DF4602", "#40B4E5", "#F29934", "#8FC7E8", "#F6C65B",
+          "#006098", "#C24D00", "#83276B", "#4C8B2B", "#E92076",
+          "#006C68", "#9E9E98", "#FFC20E", "#B687B8", "#A6CE39", "#EFB3CB",
+          "#009490", "#D5D4C7", "#FFE100"),
+  main  = c("#007CBA", "#DF4602", "#40B4E5", "#F29934", "#8FC7E8", "#F6C65B",
+            "#006098", "#C24D00"),
+  blues  = c("#006098", "#007CBA", "#40B4E5", "#40B4E5"),
+  oranges   = c("#C24D00", "#DF4602", "#F29934", "#F6C65B"),
+  contrast = c("#007CBA", "#DF4602", "#83276B", "#4C8B2B", "#E92076",
+               "#006C68", "#9E9E98", "#FFC20E", "#B687B8", "#A6CE39", "#EFB3CB",
+               "#009490", "#D5D4C7", "#FFE100")
 )
 
 
-#' Return function to interpolate a MSK color palette
+#' MSK branded colors palette generator
 #'
-#' This can take a color palette and interpolate between the specified colors
-#' to get color scales for use in continuous variable settings
-#'
-#' @param palette Character name of palette in msk_palettes. Defaults to "main"
-#' @param reverse Boolean indicating whether the palette should be reversed
-#' @param ... Additional arguments to pass to colorRampPalette()
-#'
+#' @param n Number of colors desired. If omitted, uses all colours.
+#' @param name Name of desired palette. Choices are: \code{all},
+#' \code{main} (default),  \code{blues}, \code{oranges}, \code{contrast}
+#' @param type Either "continuous" or "discrete". Use continuous if you want
+#' to automatically interpolate between colours.
+#'   @importFrom graphics rgb rect par image text
+#' @return A vector of colours.
 #' @export
+#' @keywords colors
+#' @examples
+#' msk_palette("main")
+#' msk_palette("blues")
+#' msk_palette("contrast")
 #'
-msk_pal <- function(palette = "main", reverse = FALSE, ...) {
-  pal <- msk_palettes[[palette]]
-
-  if (reverse) pal <- rev(pal)
-
-  colorRampPalette(pal, ...)
-}
-
-
-#' Color scale constructor for MSK colors
+#' library(ggplot2)
 #'
-#' @param palette Character name of palette in msk_palettes
-#' @param discrete Boolean indicating whether color aesthetic is discrete or not
-#' @param reverse Boolean indicating whether the palette should be reversed
-#' @param ... Additional arguments passed to discrete_scale() or
-#'            scale_color_gradientn(), used respectively when discrete is
-#'            TRUE or FALSE
+#' # use a single brand color from a palette
+#' ggplot(mtcars, aes(hp, mpg)) +
+#' geom_point(size = 4, color = msk_palette("main")[1])
 #'
-#' @export
+#' # use a discrete color scale
+#' ggplot(iris, aes(Sepal.Width, Sepal.Length, color = Species)) +
+#' geom_point(size = 4) +
+#' scale_color_manual(values = msk_palette("contrast"))
 #'
-scale_color_msk <- function(palette = "main", discrete = TRUE,
-                            reverse = FALSE, ...) {
-  pal <- msk_pal(palette = palette, reverse = reverse)
+#' # use a continuous color scale
+#' ggplot(iris, aes(Sepal.Width, Sepal.Length, color = Sepal.Length)) +
+#' geom_point(size = 4, alpha = .6) +
+#' scale_color_gradientn(colors = msk_palette("blues", type = "continuous"))
 
-  if (discrete) {
-    discrete_scale("colour", paste0("msk_", palette), palette = pal, ...)
-  } else {
-    scale_color_gradientn(colours = pal(256), ...)
+msk_palette <- function(name, n, type = c("discrete", "continuous")) {
+  type <- match.arg(type)
+
+  pal <- msk_palettes[[name]]
+  if (is.null(pal))
+    stop("Palette not found.")
+
+  if (missing(n)) {
+    n <- length(pal)
   }
-}
 
-
-#' Fill scale constructor for MSK colors
-#'
-#' @param palette Character name of palette in msk_palettes
-#' @param discrete Boolean indicating whether color aesthetic is discrete or not
-#' @param reverse Boolean indicating whether the palette should be reversed
-#' @param ... Additional arguments passed to discrete_scale() or
-#'            scale_fill_gradientn(), used respectively when discrete is
-#'            TRUE or FALSE
-#'
-#' @export
-#'
-scale_fill_msk <- function(palette = "main", discrete = TRUE, reverse = FALSE,
-                           ...) {
-  pal <- msk_pal(palette = palette, reverse = reverse)
-
-  if (discrete) {
-    discrete_scale("fill", paste0("msk_", palette), palette = pal, ...)
-  } else {
-    scale_fill_gradientn(colours = pal(256), ...)
+  if (type == "discrete" && n > length(pal)) {
+    stop("Number of requested colors greater than what palette can offer")
   }
+
+  out <- switch(type,
+                continuous = grDevices::colorRampPalette(pal)(n),
+                discrete = pal[1:n]
+  )
+  structure(out, class = "palette", name = name)
 }
 
+
+#' @export
+#' @importFrom graphics rect par image text
+#' @importFrom grDevices rgb
+print.palette <- function(x, ...) {
+  n <- length(x)
+  old <- par(mar = c(0.5, 0.5, 0.5, 0.5))
+  on.exit(par(old))
+
+  image(1:n, 1, as.matrix(1:n), col = x,
+        ylab = "", xaxt = "n", yaxt = "n", bty = "n")
+
+  rect(0, 0.9, n + 1, 1.1, col = rgb(1, 1, 1, 0.8), border = NA)
+  text((n + 1) / 2, 1, labels = attr(x, "name"), cex = 1, family = "serif")
+}
 
 
